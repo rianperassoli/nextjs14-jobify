833c21a21aac1ac387b3d86be9314e00cd91638c
Rian Perassoli <rian.perassoli@gmail.com>
2024-08-08 16:44:31 -0700

    editing job form created

---
+++ app/(dashboard)/jobs/[id]/page.tsx
@@ -0,0 +1,24 @@
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+import { getSingleJobAction } from "@/utils/actions";
+import { EditJobForm } from "@/components/EditJobForm";
+
+async function JobDetailPage({ params }: { params: { id: string } }) {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ["job", params.id],
+    queryFn: () => getSingleJobAction(params.id),
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <EditJobForm jobId={params.id} />
+    </HydrationBoundary>
+  );
+}
+
+export default JobDetailPage;
